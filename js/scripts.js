52617dbf9382ed0ae9bdbbb7a7800d41580299e6
Mnjeri <millynjesh@gmail.com>
2017-06-16 17:57:56 +0300

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,58 @@
-//business logic
-// function player A(randomA) {
-// 	this.randomA = randomA;
-	
+function playerA(randomA){
+  this.randomA=randomA;
+}
+function playerB(randomB){
+  this.randomB=randomB;
+}
+// //prototypes
+// playerA.prototype.scoreA=function(){
+//   return this.randomA;
 // }
-
-// function player B(randomB) {
-// 	this.randomB= randomB
+// playerB.prototype.scoreB=function(){
+//   return this.randomB;
 // }
+//playerA
+$(document).ready(function(){
+  
+  var newPlayerA=new playerA(playerOne);
+  var playerOne=0;
+  var newPlayerB=new playerB(playerTwo);
+  var playerTwo=0;
+  $("form#One").submit(function(event){
+    event.preventDefault();
+    var randomNumberA=Math.floor(Math.random() * 6) + 1;
+    if(randomNumberA===1){
+      alert("sorry, you have rolled 1");
+      $("button#buttonA").prop('disabled',true);
+      $("button#buttonB").prop('disabled',false);
+      
+    }
+    else{
+        playerOne+=randomNumberA;
+      }
+    console.log(playerA);
+      //$("p#1").text(playerOne);
+     
+  });
+  $("form#Two").submit(function(event){
+    event.preventDefault();
+    var randomNumberB=Math.floor(Math.random() * 6) + 1;
+    if(randomNumberB===1){
+      alert("sorry, you have rolled 1");
+      $("button#buttonB").prop('disabled',true);
+      $("button#buttonA").prop('disabled',false);
+      
+    }
+    else{
+      playerTwo+=randomNumberB;
+    }
+console.log(playerB);
 
-// playerA.prototype.scoreA =function() {
-// 	return scoreA.randomA;
-// }
+//$("p#2").text(playerTwo);
+    
+    
+  });
+});
 
-// playerB.prototype.scoreB = function() {
-// 	return scoreB.randomB;
-// }
-
-
-
-
-$(document).ready(function() {
-
-	var newplayerA=(player one);
-	var playerone =0;
-
-    $("form#"new playerA).submit(function(event) {
-      event.preventDefault();
-      var randomNumber A =Math.floor(Math.random() * 6) + 1;
-        if(randomNumber A===1){
-        	alert("win!!");
-        }
-        else{
-        	playerone += randomNumber A;
-        }
-        console.log(playerone);
-
-
-        };
-    )};
 
 
