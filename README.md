c75f7f3fa7532dd6be2877370530daca2900b4ac
Mnjeri <millynjesh@gmail.com>
2017-06-16 16:49:58 +0300

    added changes

---
+++ README.md
@@ -0,0 +1,10 @@
+# author name
+Miriam Njeri
+
+# Project or Program name
+Pig Dice Website
+
+# desciption of project
+ it is aprogram where two users can play Pig dice against each other using JavaScript programming skills such as constructors, prototypes, objects, properties, methods, jQuery and DOM manipulation and traversal.
+
+# specifications
---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+//business logic
+// function player A(randomA) {
+// 	this.randomA = randomA;
+	
+// }
+
+// function player B(randomB) {
+// 	this.randomB= randomB
+// }
+
+// playerA.prototype.scoreA =function() {
+// 	return scoreA.randomA;
+// }
+
+// playerB.prototype.scoreB = function() {
+// 	return scoreB.randomB;
+// }
+
+
+
+
+$(document).ready(function() {
+
+	var newplayerA=(player one);
+	var playerone =0;
+
+    $("form#"new playerA).submit(function(event) {
+      event.preventDefault();
+      var randomNumber A =Math.floor(Math.random() * 6) + 1;
+        if(randomNumber A===1){
+        	alert("win!!");
+        }
+        else{
+        	playerone += randomNumber A;
+        }
+        console.log(playerone);
+
+
+        };
+    )};
+
+
