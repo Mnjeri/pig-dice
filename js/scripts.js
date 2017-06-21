ad2ebe960e4615854e1ea6a1137f57cbcc9d45fa
Mnjeri <millynjesh@gmail.com>
2017-06-21 15:53:18 +0300

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-function Players1(points){
+function PlayersA(points){
     this.points = points;
 }
-Players1.prototype.computeScores = function(randomValue){
+PlayersA.prototype.calculateScores = function(randomValue){
     while (randomValue > 0){
-       $('#name1').prop('disabled', true);
+       $('#nameOne').prop('disabled', true);
     if (randomValue === 1){
         alert("You have burnt");
         $('#Button').prop('disabled', true);
@@ -17,25 +17,25 @@ Players1.prototype.computeScores = function(randomValue){
     }
 };
 
-Players1.prototype.holdCards = function(){
+PlayersA.prototype.holdCards = function(){
      $('#Button').prop('disabled', true);
      $('#Button1').prop('disabled', false);
      alert("You have hold your cards at "+ this.points);
 };
-Players1.prototype.winner = function(){
+PlayersA.prototype.winner = function(){
    if(this.points >= 100){
-        alert("You are the winner");
+        alert("You have won");
         $('#Button').prop('disabled', true);
         $('#Button1').prop('disabled', true);
         
     } 
 };
-function Players2(points){
+function PlayersB(points){
     this.points = points;
 }
-Players2.prototype.computeScores = function(randomValue){
+PlayersB.prototype.calculateScores = function(randomValue){
     while(randomValue > 0){
-         $('#name2').prop('disabled', true);
+         $('#nameTwo').prop('disabled', true);
         if (randomValue ===1){
             alert("You have Burnt");
             $('#Button').prop('disabled', false);
@@ -46,14 +46,14 @@ Players2.prototype.computeScores = function(randomValue){
         }
     }
 };
-Players2.prototype.holdCards = function(){
+PlayersB.prototype.holdCards = function(){
      $('#Button').prop('disabled', false);
      $('#Button1').prop('disabled', true);
      alert("You have hold your cards at "+ this.points);
 };
-Players2.prototype.winner = function(){
+PlayersB.prototype.winner = function(){
    if(this.points >= 100){
-        alert("You are the winner");
+        alert("You have won");
         $('#Button').prop('disabled', true);
         $('#Button1').prop('disabled', true);
         
@@ -66,27 +66,27 @@ $(document).ready(function(){
     var result  = new Players1(0);
     //An object of player2
     var result1  = new Players2(0);
-    $('form#player1').submit(function(event){
+    $('form#playerOne').submit(function(event){
         event.preventDefault();
         var randomm = Math.floor(Math.random() * 6) + 1;
-        var name1 = $('input#name1').val();
+        var name1 = $('input#nameOne').val();
         
         
         
-        $('#name11').text("Name: " + name1);
+        $('#name12').text("Name: " + name1);
         $('#numbers1').text(result.points);
         
         result.computeScores(randomm);
         result.winner();
         
         });
-    $('form#player2').submit(function(event){
+    $('form#playerTwo').submit(function(event){
         event.preventDefault();
         var random2 = Math.floor(Math.random() * 6) + 1;
-        var name2 = $('input#name2').val();
+        var name2 = $('input#nameTwo').val();
         
          
-        $('#name12').text("Name: " + name2);
+        $('#name14').text("Name: " + name2);
         $('#numbers2').text(result1.points);
         
         result1.computeScores(random2);
